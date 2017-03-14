7e50352093c2730c8e7b218f99b3b608cd717464
dmfenton <dfenton@esri.com>
2017-03-13 22:30:04 -0400

    update for koop 3

--- HEROKU.md
+++ HEROKU.md
@@ -4,36 +4,6 @@ If you're short on time: [![Deploy](https://www.herokucdn.com/deploy/button.svg)
 
 From [Heroku Dev Center - Extensions, PostGIS...](https://devcenter.heroku.com/articles/heroku-postgres-extensions-postgis-full-text-search#postgis) (accessed on 3/23/2016):
 
-> Currently, PostGIS can only be used on Production tier Heroku Postgres plans and on hobby plans.
-
-If you'd like to learn more, below we explain what we did differently in the [heroku](https://github.com/koopjs/koop-sample-app/tree/heroku) branch to deploy koop with an IN_MEMORY cache so that we can utilize a free plan.  
-
-## Instructions
-
-A PostGIS store *is* [recommended](http://koopjs.github.io/docs/caches.html) in production, but an IN_MEMORY store can be configured by following the steps below:
-
-1. Remove mention of a PostGIS database instance from `config/default.json`
-
-```js
-
-// get rid of this part
-"db": {
-  "conn": "postgres://localhost/koopdev"
-}
-```
-2. Comment out the code which registers the PostGIS cache with koop in `index.js`
-
-```js
-// comment out this stuff
-var pgCache = require('koop-pgcache')
-
-// and a little further down...
-
-koop.registerCache(pgCache)
-```
-
-That's it!
-
 ## Resources
 
 * [Koop](https://github.com/koopjs/koop)
--- server.js
+++ server.js
@@ -1,72 +1,24 @@
-var path = require('path')
-var cors = require('cors')
-var config = require('config')
-var logger = require('morgan')
-var errorHandler = require('errorhandler')
-var express = require('express')
-var koop = require('koop')(config)
-
-// cache
-var pgCache = require('koop-pgcache')
+const config = require('config')
+const Koop = require('koop')
+const koop = new Koop(config)
 
 // providers
-var agol = require('koop-agol')
-var gist = require('koop-gist')
-var github = require('koop-github')
-var socrata = require('koop-socrata')
-
-var app = express()
-var server
-
-// use HTTPS if it's included in the configuration
-if (config.https_server &&
-  config.https_server.private_key &&
-  config.https_server.public_key) {
-  var https = require('https')
-  var fs = require('fs')
-  var options = {
-    key: fs.readFileSync(config.https_server.private_key),
-    cert: fs.readFileSync(config.https_server.public_key)
-  }
-} else {
-  // use http module by default
-  var http = require('http')
-}
-
-// register PostGIS cache
-koop.registerCache(pgCache)
+const agol = require('koop-agol')
+const zillow = require('koop-zillow')
+const craigslist = require('koop-craigslist')
+const trimet = require('koop-trimet')
 
 // register koop providers
 koop.register(agol)
-koop.register(gist)
-koop.register(github)
-koop.register(socrata)
+koop.register(zillow)
+koop.register(craigslist)
+koop.register(trimet)
 
-app.set('port', process.env.PORT || config.server.port || 1337)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.use(cors()) // add CORS support (recommended)
-app.use(koop) // add koop middleware
-
-if (app.get('env') === 'production') {
-  app.use(logger('combined'))
-} else {
-  app.use(logger('dev'))
-  app.use(errorHandler())
-}
-
-app.get('/', function (req, res) {
+// This is how you implement additional arbitrary routes on the Koop server
+koop.server.get('/', function (req, res) {
   res.render('index', { status: koop.status })
 })
 
-if (config.https_server) {
-  server = https.createServer(options, app)
-} else {
-  server = http.createServer(app)
-}
-
+const port = config.port || 8080
 // start the server
-server.listen(app.get('port'), function () {
-  koop.log.info('Koop %s server listening at %s', app.get('env'), app.get('port'))
-})
+koop.server.listen(port, () => koop.log.info(`Koop server listening at ${port}`))
